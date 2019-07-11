e1deaf333c3e51e0e84d0c12facae2a82761cfd3
Remi Papillier <remi-papillier@hotmail.fr>
2019-07-11 15:10:52 +0200

    redirects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'CV', component: CVComponent}
+  {path: 'cv', component: CVComponent}
 ];
 
 @NgModule({

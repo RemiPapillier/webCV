4359f87bc99d52ac937492507d31a987954bad85
Remi Papillier <remi-papillier@hotmail.fr>
2019-07-11 14:48:04 +0200

    cv comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CVComponent } from './cv/cv.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CVComponent},
+  {path: 'CV', component: CVComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
